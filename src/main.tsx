efef6f54239562ff2d636fa2e557ccd4f19fb3cb
Harsh Singh <83687626+HarshNeymar@users.noreply.github.com>
2026-05-20 12:26:14 +0000

    x

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,12 @@ import { router } from "./router";
 
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
-);
+const rootElement = document.getElementById("root");
+
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  );
+}
